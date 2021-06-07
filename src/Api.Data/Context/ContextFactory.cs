21e7bba60143b80214c73db362c999be15e9ca05
thiago-desenv <thiago_osim@hotmail.com>
2021-06-07 19:26:24 -0300

    Finalizada migração do banco de dados para SQLServer

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -8,11 +8,11 @@ namespace Api.Data.Context
         public MyContext CreateDbContext(string[] args)
         {
             //Usado para criar as migrações
-            var connectionStringMySql = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=1234567";
-            //var connectionStringSqlServer = "Server=WINAP9PSX6DQSD3;Initial Catalog=dbiapi;MultipleActiveResultSets=true;User ID=txf;Password=robo@0101;";
+            //var connectionStringMySql = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=1234567";
+            var connectionStringSqlServer = @"Data Source=DESKTOP-2GT602A\SQLEXPRESS;Initial Catalog=dbapi;Integrated Security=True";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
-            optionsBuilder.UseMySql(connectionStringMySql);
-            //optionsBuilder.UseSqlServer(connectionStringSqlServer);
+            //optionsBuilder.UseMySql(connectionStringSqlServer);
+            optionsBuilder.UseSqlServer(connectionStringSqlServer);
             return new MyContext(optionsBuilder.Options);
         }
     }

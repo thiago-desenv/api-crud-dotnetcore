2540f879ab5a65baca4938163ad3fcf69ff37394
Thiago Xavier Ferreira <t.xavier.ferreira@avanade.com>
2021-06-07 10:43:39 -0300

    Incluindo o uso do banco de dados SQLServer na aplicação

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -8,9 +8,11 @@ namespace Api.Data.Context
         public MyContext CreateDbContext(string[] args)
         {
             //Usado para criar as migrações
-            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=1234567";
+            //var connectionStringMySql = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=1234567";
+            var connectionStringSqlServer = "Server=WINAP9PSX6DQSD3;Initial Catalog=dbiapi;MultipleActiveResultSets=true;User ID=txf;Password=robo@0101;";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
-            optionsBuilder.UseMySql(connectionString);
+            //optionsBuilder.UseMySql(connectionStringMySql);
+            optionsBuilder.UseSqlServer(connectionStringSqlServer);
             return new MyContext(optionsBuilder.Options);
         }
     }

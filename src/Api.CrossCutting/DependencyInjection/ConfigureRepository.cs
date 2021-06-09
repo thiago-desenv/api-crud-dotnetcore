3f7db53ef5cab84226198b3dba0a1774f2e2ab1d
thiago-desenv <thiago_osim@hotmail.com>
2021-06-09 20:44:13 -0300

    Criado as variáveis de banco de dados no launch.json para distinguir os bancos de dados

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using Api.Data.Context;
 using Api.Data.Implementations;
 using Api.Data.Repository;
@@ -15,10 +16,20 @@ namespace Api.CrossCutting.DependencyInjection
             serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
             serviceCollection.AddScoped<IUserRepository, UserImplementation>();
 
-            serviceCollection.AddDbContext<MyContext>(
-                options => options.UseSqlServer(@"Data Source=DESKTOP-2GT602A\SQLEXPRESS;Initial Catalog=dbapi;Integrated Security=True")
-            //options => options.UseMySql("Server=localhost;Port=3306;Database=xxx;Uid=root;Pwd=xxx")
-            );
+            if (Environment.GetEnvironmentVariable("DATABASE").ToUpper() == "SQLSERVER")
+            {
+                serviceCollection.AddDbContext<MyContext>
+                (
+                    options => options.UseSqlServer(Environment.GetEnvironmentVariable("DB_CONNECTION_SQLSERVER"))
+                );
+            }
+            else
+            {
+                serviceCollection.AddDbContext<MyContext>
+                (
+                    options => options.UseMySql(Environment.GetEnvironmentVariable("DB_CONNECTION_MYSQL"))
+                );
+            }
         }
     }
 }

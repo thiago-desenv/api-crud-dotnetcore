521ab2b08eb41e3b07bdec2c6e835dd2638584bf
thiago-desenv <thiago_osim@hotmail.com>
2021-06-09 23:30:52 -0300

    Criado a classe BaseTest para configuração dos testes que serão realizados

---
+++ src/Api.Data.UnitaryTests/BaseTest.cs
@@ -0,0 +1,44 @@
+using System;
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitaryTests
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+
+        }
+    }
+
+    public class DbTest : IDisposable
+    {
+        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTest()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(o =>
+                o.UseSqlServer($"Persist Security Info=True;Data Source=DESKTOP-2GT602A\\SQLEXPRESS;Database={dataBaseName};Integrated Security=True"),
+                ServiceLifetime.Transient
+            );
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}

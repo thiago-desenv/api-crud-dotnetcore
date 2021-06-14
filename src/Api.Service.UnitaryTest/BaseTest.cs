688b437f1257d7d29e5ded2782328adca2f6dc20
thiago-desenv <thiago_osim@hotmail.com>
2021-06-14 16:03:45 -0300

    Configurado o AutoMapper para testes unitários do projeto Api.Service

---
+++ src/Api.Service.UnitaryTest/BaseTest.cs
@@ -0,0 +1,34 @@
+using System;
+using Api.CrossCutting.Mappings;
+using AutoMapper;
+
+namespace Api.Service.UnitaryTest
+{
+    public abstract class BaseTest
+    {
+        public IMapper Mapper { get; set; }
+
+        public BaseTest()
+        {
+            Mapper = new AutoMapperFixture().GetMapper();
+        }
+    }
+
+    public class AutoMapperFixture : IDisposable
+    {
+        public IMapper GetMapper()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ModelToEntityProfile());
+                cfg.AddProfile(new DTOToModelProfile());
+                cfg.AddProfile(new EntityToDTOProfile());
+            }).CreateMapper();
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}

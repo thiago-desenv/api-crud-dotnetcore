44409dcdc7c3f5b77479a0bfdc70a41a1cae2aed
thiago-desenv <thiago_osim@hotmail.com>
2021-06-20 03:32:00 -0300

    Criado a classe que rertonará os dados de autenticação para prosseguir com os testes integrados

---
+++ src/Api.Integration.Test/LoginResponseDTO.cs
@@ -0,0 +1,23 @@
+namespace Api.Integration.Test
+{
+    public class LoginResponseDTO
+    {
+        [JsonProperty("authenticated")]
+        public bool authenticated { get; set; }
+
+        [JsonProperty("created")]
+        public DateTime created { get; set; }
+
+        [JsonProperty("expiration")]
+        public DateTime expiration { get; set; }
+
+        [JsonProperty("acessToken")]
+        public string acessToken { get; set; }
+
+        [JsonProperty("userName")]
+        public string userName { get; set; }
+
+        [JsonProperty("message")]
+        public string message { get; set; }
+    }
+}
